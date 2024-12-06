ef7fd92a0d977efa68a15e2899998800877be44f
Nathalie Lysefeldt <lysefeldt@outlook.com>
2024-12-06 11:10:52 +0100

    changed portfolio cv, added modal of video noodletime, script and css for it

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,18 @@ function toggleMenu() {
 	menu.classList.toggle("open");
 	icon.classList.toggle("open");
 }
+function openModal() {
+	document.getElementById("noodletimeModal").style.display = "block";
+}
+
+function closeModal() {
+	document.getElementById("noodletimeModal").style.display = "none";
+}
+
+// Stänger modalen om användaren klickar utanför innehållet
+window.onclick = function (event) {
+	const modal = document.getElementById("noodletimeModal");
+	if (event.target === modal) {
+		modal.style.display = "none";
+	}
+};
